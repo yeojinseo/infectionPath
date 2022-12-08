4ee9eb8ee616c2cdabb3065c7c774cc9403afea2
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-12-08 17:31:47 +0900

    Initial commit

---
+++ ifct_element.c
@@ -0,0 +1,173 @@
+//
+//  ifs_element.c
+//  InfestPath
+//
+//  Created by Juyeop Kim on 2020/10/20.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "ifct_element.h"
+
+typedef enum place {
+    Seoul,          //0
+    Jeju,           //1
+    Tokyo,          //2
+    LosAngeles,     //3
+    NewYork,        //4
+    Texas,          //5
+    Toronto,        //6
+    Paris,          //7
+    Nice,           //8
+    Rome,           //9
+    Milan,          //10
+    London,         //11
+    Manchester,     //12
+    Basel,          //13
+    Luzern,         //14
+    Munich,         //15
+    Frankfurt,      //16
+    Berlin,         //17
+    Barcelona,      //18
+    Madrid,         //19
+    Amsterdam,      //20
+    Stockholm,      //21
+    Oslo,           //22
+    Hanoi,          //23
+    Bangkok,        //24
+    KualaLumpur,    //25
+    Singapore,      //26
+    Sydney,         //27
+    SaoPaulo,       //28
+    Cairo,          //29
+    Beijing,        //30
+    Nairobi,        //31
+    Cancun,         //32
+    BuenosAires,    //33
+    Reykjavik,      //34
+    Glasgow,        //35
+    Warsow,         //36
+    Istanbul,       //37
+    Dubai,          //38
+    CapeTown        //39
+} place_t;
+
+char countryName[N_PLACE+1][MAX_PLACENAME] =
+{   "Seoul",
+    "Jeju",
+    "Tokyo",
+    "LosAngeles",
+    "NewYork",
+    "Texas",
+    "Toronto",
+    "Paris",
+    "Nice",
+    "Rome",
+    "Milan",
+    "London",
+    "Manchester",
+    "Basel",
+    "Luzern",
+    "Munich",
+    "Frankfurt",
+    "Berlin",
+    "Barcelona",
+    "Madrid",
+    "Amsterdam",
+    "Stockholm",
+    "Oslo",
+    "Hanoi",
+    "Bangkok",
+    "KualaLumpur",
+    "Singapore",
+    "Sydney",
+    "SaoPaulo",
+    "Cairo",
+    "Beijing",
+    "Nairobi",
+    "Cancun",
+    "BuenosAires",
+    "Reykjavik",
+    "Glasgow",
+    "Warsow",
+    "Istanbul",
+    "Dubai",
+    "CapeTown",
+    "Unrecognized"
+};
+
+
+typedef struct ifct_ele {
+	int pIndex; //number
+	int age; //age
+	int time; //time
+	place_t placeHist[N_HISTORY]; //place(N_HISTORY개)
+} ifct_ele_t;
+
+
+
+void* ifctele_genElement(int index, int age, unsigned int detected_time, int history_place[N_HISTORY])
+{
+	ifct_ele_t* ptr;
+	
+	ptr = (int*)malloc(sizeof(ifct_ele_t));
+	ptr->pIndex = index;
+	ptr->age = age;
+	ptr->time = detected_time;
+	ptr->placeHist[N_HISTORY] = history_place[N_HISTORY]; //(ifct_ele_t.placeHist, ) ??
+
+	//여기에서 free하면 안됨. 
+	return ptr;
+}
+
+
+
+int ifctele_getAge(void* obj)
+{
+	ifct_ele_t* ptr = (ifct_ele_t*)obj;
+	
+	return ptr->age;
+}
+
+
+
+int ifctele_getHistPlaceIndex(void* obj, int pIndex) //place는 몇번쨰 장소인지도 고려. 
+{
+	
+}
+
+
+
+unsigned int ifctele_getinfestedTime(void* obj)
+{
+	
+}
+
+
+
+void ifctele_printElement(void* obj)
+{
+	int i;
+	ifct_ele_t* ptr = (ifct_ele_t*)obj;
+	
+	printf("Patient Index : %i\n", ptr->pIndex);
+	printf("Age : %i\n", ptr->age);
+	printf("Detected Time : %i\n", ptr->time);
+//	printf("Place : %s\n", ptr->placeHist); //장소어케함?
+	for(i=0;i<5;i++)
+	{
+		ifct_ele_t.ptr[i]->placeHist[i];
+	}
+	//...
+}
+
+
+
+//return country name pointer from country number
+char* ifctele_getPlaceName(int placeIndex)
+{
+	return countryName[placeIndex];
+}
+
---
+++ main.c
@@ -0,0 +1,141 @@
+//
+//  main.c
+//  infestPath
+//
+//  Created by Juyeop Kim on 2022/10/20.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include "ifct_element.h"
+#include "ifct_database.h"
+
+#define MENU_PATIENT        1
+#define MENU_PLACE          2
+#define MENU_AGE            3
+#define MENU_TRACK          4
+#define MENU_EXIT           0
+
+#define TIME_HIDE           2
+
+
+int trackInfester(int patient_no, int *detected_time, int *place);
+int main(int argc, const char * argv[]) {
+    
+    int menu_selection;
+    void *ifct_element;
+    FILE* fp;
+    int pIndex, age, time;
+    int placeHist[N_HISTORY];
+
+    
+    //------------- 1. loading patient info file ------------------------------
+    //1-1. FILE pointer open
+    if (argc != 2)
+    {
+        printf("[ERROR] syntax : infestPath (file path).");
+        return -1;
+    }
+    
+    fp = fopen(argv[1],"r");
+    if (fp == NULL)
+    {
+        printf("[ERROR] Failed to open database file!! (%s)\n", argv[1]);
+        return -1;
+    }
+    
+    //1-2. loading each patient informations
+    fp = fopen("patientInfo.txt", "r");
+    
+    while ( 3 == fscanf(fp, "%d %d %d", &pIndex, &age, &time))
+    {
+    	int i;
+    	for (i=0;i<5;i++)
+    	{
+    		fscanf(fp, "%d", &placeHist[i]);
+//    		printf("%5d", placeHist[i]);
+    	}
+    	
+    	ifct_element = ifctele_genElement(pIndex, age, time, placeHist);
+    	ifctdb_addTail(ifct_element);
+    	
+		ifctele_printElement(ifct_element); //프린트해보기.    	
+//		ifctele_getAge(ifct_element); //프린트해보기. 
+    	
+//    	printf("\n"); //
+	}
+	
+/*
+   {
+    	int place1, place2; //첫번쨰, 두번쨰 장소 
+    	
+    	place1 = 3; //Los Angeles 
+		place2 = 15; //Munich
+		
+		printf("The first place is %s\n", ifctele_getPlaceName(place1));
+		printf("The second place is %s\n", ifctele_getPlaceName(place2));
+	}
+
+	{
+		int i, j;
+		for (i=0;i<5;i++)
+		{
+			printf("%d 번째 환자 감염 경로 : ", i);
+			for (j=0;j<5;j++)
+				printf("%s ", ifctele_getPlaceName(placeHist[j]));
+				printf("\n");
+		}
+		printf("\n");
+	}
+*/
+
+	
+    //1-3. FILE pointer close //10주차 화 
+    fclose(fp);
+	
+    do {
+        printf("\n=============== INFECTION PATH MANAGEMENT PROGRAM (No. of patients : %i) =============\n", ifctdb_len());
+        printf("1. Print details about a patient.\n");                      //MENU_PATIENT
+        printf("2. Print list of patients infected at a place.\n");        //MENU_PLACE
+        printf("3. Print list of patients in a range of age.\n");          //MENU_AGE
+        printf("4. Track the root of the infection\n");                     //MENU_TRACK
+        printf("0. Exit.\n");                                               //MENU_EXIT
+        printf("=============== ------------------------------------------------------- =============\n\n");
+        
+        printf("Select a menu :");
+        scanf("%d", &menu_selection);
+        fflush(stdin);
+        
+        switch(menu_selection)
+        {
+            case MENU_EXIT:
+                printf("Exiting the program... Bye bye.\n");
+                break;
+                
+            case MENU_PATIENT:
+                ifct_element = ifctele_genElement(pIndex, age, time, placeHist);
+                ifctele_printElement(ifct_element);
+                break;
+                
+            case MENU_PLACE:
+                
+                break;
+                
+            case MENU_AGE:
+                
+                break;
+                
+            case MENU_TRACK:
+                    
+                break;
+                
+            default:
+                printf("[ERROR Wrong menu selection! (%i), please choose between 0 ~ 4\n", menu_selection);
+                break;
+        }
+    
+    } while(menu_selection != 0);
+    
+    
+    return 0;
+}
